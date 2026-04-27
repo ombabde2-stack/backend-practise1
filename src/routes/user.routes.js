c5a90f7483f51d2e64090d165d2932ee585bdcb1
Om Babde <ombabde2@gmail.com>
2026-04-27 09:48:58 +0530

    access Token refreshed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,10 @@
 import {Router} from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { loginUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { logoutUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { refreshAccessToken } from "../controllers/user.controller.js";
 
 const router = Router();
 
@@ -17,6 +21,10 @@ router.route("/register").post(
     ]),
     registerUser
 );
-// router.route("/login").post(loginUser);
+router.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router; 
