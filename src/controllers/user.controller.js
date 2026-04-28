17a473d22ca96ca429dd7385f94592ce3a204a8d
Om Babde <ombabde2@gmail.com>
2026-04-28 10:41:01 +0530

    add management endpoints to user account

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -214,6 +214,7 @@ const changeCurrentPassword = asyncHandler(async(req,res) => {
     }
 
     user.password = newPassword;
+    await user.save({validateBeforeSave: false})
 
     return res
        .status(200)      
@@ -222,8 +223,73 @@ const changeCurrentPassword = asyncHandler(async(req,res) => {
        )
 })
 
+const getCurrentUser = asyncHandler(async(req,res) => {
+    return res
+    .status(200)
+    .json(200, req.user, "current account fetched successsfully")
+})
+
+const updateAccountDetails = asyncHandler(async(req,res) => {
+
+    const {fullName, email} = req.body
+    if(!fullName || !email){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName: fullName,
+                email: email
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user,"Account details updated successfully"))
+
+})
+
+const updateUserAvatar = asyncHandler(async(req,res) => {
+
+    const avatarLocalPath = req.file?.path 
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is missing")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    if(!avatar.url){
+       throw new ApiError(400, "Error while uploading the avatar")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage: coverImage.url
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+    return res.status(200)
+              .json(new ApiResponse(200, "Cover Image updated successfully"))
+
+}).select("-password")
+
 export {registerUser,
         loginUser,
         logoutUser,
-        refreshAccessToken
+        refreshAccessToken,
+        changeCurrentPassword,
+        getCurrentUser,
+        updateAccountDetails,
+        updateUserAvatar
 }
