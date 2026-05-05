ac49736821f479881bb5418b1c9ad927178ac15a
Om Babde <ombabde2@gmail.com>
2026-05-05 16:53:59 +0530

    login problem solved

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,6 +18,7 @@ const generateAccessAndRefreshTokens = async(userId) => {
 
     } 
     catch (error) {
+        console.log("Token generation failed:", error?.message);
         throw new ApiError(500, "Something went wrong while generating referesh and access token")
     }
 }
@@ -86,8 +87,12 @@ const loginUser = asyncHandler(async(req,res) => {
     
     const {email,username,password} = req.body
 
-    if(!username || !email) {
-        throw new ApiError(400, "username or password require")
+    if(!(username || email)) {
+        throw new ApiError(400, "username or email is required")
+    }
+
+    if(!password) {
+        throw new ApiError(400, "password is required")
     }
 
     const user = await User.findOne({
