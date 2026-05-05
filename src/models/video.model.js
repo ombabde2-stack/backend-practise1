eeac96eddddd87f2fe51bf210f785ec854f6e847
Om Babde <ombabde2@gmail.com>
2026-05-06 00:00:47 +0530

    added tweet, playlist, comment, like model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -40,4 +40,4 @@ const videoSchema = new mongoose.Schema({
 
 videoSchema.plugin(mongooseAggregatePaginate);
 
-export const video = mongoose.model("Video",videoSchema);
+export const Video = mongoose.model("Video",videoSchema);
